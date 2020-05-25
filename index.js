a99a3e1b3cd0666e67e567945f00b4df0954e9d3
abhijeetk698 <abhijeetk698@gmail.com>
2020-05-25 23:42:38 +0530

    added logout and isLoggedIn functionality

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.get("/",(req,res)=>{
     res.render("home");    
 });
 
-app.get("/secret",(req,res)=>{
+app.get("/secret",isLoggedIn,(req,res)=>{
     res.render("secret");
 });
 
@@ -75,6 +75,18 @@ app.post("/login",passport.authenticate("local",{
     failureRedirect : "/login"
 }),(req,res)=>{})
 
+// LOG OUT ROUTE
+
+app.get("/logout",(req,res)=>{
+    req.logout();
+    res.redirect("/");
+});
+
+function isLoggedIn(req,res,next){
+    if(req.isAuthenticated()){
+        return next();
+    }res.redirect("/login");
+}
 
 app.listen(3000,()=>{
     console.log("Server has started ....")
